3a84a763d591efc52c9c4f60cf4882bc6f7e8c8a
PeskyWabbit <jpeel132@gmail.com>
2019-12-03 15:03:25 -0600

    fuck you

--- EveryTrade/Source.cpp
+++ EveryTrade/Source.cpp
@@ -13,7 +13,12 @@
 
 
 NxCoreClass nxCoreClass;
-
+int startHour = 0;
+int endHour = 0;
+int startMinutes = 0;
+int endMinutes = 0;
+std::string file = "";
+std::string ticker = "";
 void getSymbol(const NxCoreMessage* pNxCoreMsg, char* Symbol)
 {
 	// Is this a valid option?
@@ -51,61 +56,87 @@ int __stdcall nxCoreCallback(const NxCoreSystem* pNxCoreSys, const NxCoreMessage
 	switch (pNxCoreMessage->MessageType)
 	{
 	
-	case NxMSG_TRADE:
-	{
-		FILE* outfile;
-		outfile = std::fopen("output1.txt", "w");
-		const NxCoreTrade& nt = pNxCoreMessage->coreData.Trade;
-		const NxCoreHeader& ch = pNxCoreMessage->coreHeader;
-		const NxTime& t = pNxCoreSys->nxTime;
-		const char* lexchange = nxCoreClass.GetDefinedString(NxST_EXCHANGE, ch.ListedExg);
-		const char* rexchange = nxCoreClass.GetDefinedString(NxST_EXCHANGE, ch.ReportingExg);
-		#pragma omp critical 
+		case NxMSG_TRADE:
 		{
+			FILE* outfile;
+			outfile = std::fopen("C:\\Users\\Nanex1\\Documents\\output1.txt", "a");
+			const NxCoreTrade& nt = pNxCoreMessage->coreData.Trade;
+			const NxCoreHeader& ch = pNxCoreMessage->coreHeader;
+			const NxTime& t = pNxCoreSys->nxTime;
+			const char* lexchange = nxCoreClass.GetDefinedString(NxST_EXCHANGE, ch.ListedExg);
+			const char* rexchange = nxCoreClass.GetDefinedString(NxST_EXCHANGE, ch.ReportingExg);
+
 			char symbol[23];
-			getSymbol(pNxCoreMessage, symbol);
-			fprintf(outfile, "%.2d:%.2d:%.2d.%.3d, Reporting:%s, Listed:%s, %s, Price(%ld@%.2lf), O(%.2lf), H(%.2lf), L(%.2lf), C(%.2lf), V(%I64d), Net(%.2lf)\n",
-				(int)t.Hour, (int)t.Minute, (int)t.Second, (int)t.Millisecond,
-				rexchange,
-				lexchange,
-				symbol,
-				nt.Size,
-				nxCoreClass.PriceToDouble(nt.Price, nt.PriceType),
-				nxCoreClass.PriceToDouble(nt.Open, nt.PriceType),
-				nxCoreClass.PriceToDouble(nt.High, nt.PriceType),
-				nxCoreClass.PriceToDouble(nt.Low, nt.PriceType),
-				nxCoreClass.PriceToDouble(nt.Last, nt.PriceType),
-				nt.TotalVolume,
-				nxCoreClass.PriceToDouble(nt.NetChange, nt.PriceType));
+			int lineHour = t.Hour;
+			int lineMinutes = t.Minute;
+			std::string lineTicker = (std::string) ch.pnxStringSymbol->String;
+			if (lineTicker != ticker || (lineHour < startHour) || (lineHour > endHour)) {
+				std::cout << (std::string) ch.pnxStringSymbol->String << " " << ticker << std::endl;
+				std::fclose(outfile);
+				break;
+			}
+			if ((startHour == lineHour && lineMinutes >= startMinutes) ||
+				(endHour == lineHour && lineMinutes <= endMinutes) ||
+				(lineHour > startHour&& lineHour < endHour)) {
+				std::cout << "YEEEEEEEEEEEEEEEEEEEEEEET" << std::endl;
+				getSymbol(pNxCoreMessage, symbol);
+				fprintf(outfile, "%.2d:%.2d:%.2d.%.3d, Reporting:%s, Listed:%s, %s, Price(%ld@%.2lf), O(%.2lf), H(%.2lf), L(%.2lf), C(%.2lf), V(%I64d), Net(%.2lf)\n",
+					(int)t.Hour, (int)t.Minute, (int)t.Second, (int)t.Millisecond,
+					rexchange,
+					lexchange,
+					symbol,
+					nt.Size,
+					nxCoreClass.PriceToDouble(nt.Price, nt.PriceType),
+					nxCoreClass.PriceToDouble(nt.Open, nt.PriceType),
+					nxCoreClass.PriceToDouble(nt.High, nt.PriceType),
+					nxCoreClass.PriceToDouble(nt.Low, nt.PriceType),
+					nxCoreClass.PriceToDouble(nt.Last, nt.PriceType),
+					nt.TotalVolume,
+					nxCoreClass.PriceToDouble(nt.NetChange, nt.PriceType));
+				std::fclose(outfile);
+			}
 		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	}
 	
-	case NxMSG_EXGQUOTE:
-	{
-		FILE* outfile;
-		outfile = std::fopen("output1.txt", "w");
-		const NxCoreQuote& nq = pNxCoreMessage->coreData.ExgQuote.coreQuote;
-		const NxCoreHeader& ch = pNxCoreMessage->coreHeader;
-		const NxTime& t = pNxCoreSys->nxTime;
-		const char* exchange = nxCoreClass.GetDefinedString(NxST_EXCHANGE, ch.ReportingExg);
-		const char* listedexg = nxCoreClass.GetDefinedString(NxST_EXCHANGE, ch.ListedExg);
-		char symbol[23];
-		getSymbol(pNxCoreMessage, symbol);
-		fprintf(outfile, "%.2d:%.2d:%.2d.%.3d, Reporting:%s, Listed:%s, %s, Ask(%ld@%.2lf), Bid(%ld@%.2lf), AskChange(%.2lf), BidChange(%.2lf), AskSizeChange(%.2lf), BidSizeChange(%.2lf)\n",
-			(int)t.Hour, (int)t.Minute, (int)t.Second, (int)t.Millisecond,
-			exchange,
-			listedexg,
-			symbol,
-			nq.AskSize,
-			nxCoreClass.PriceToDouble(nq.AskPrice, nq.PriceType),
-			nq.BidSize,
-			nxCoreClass.PriceToDouble(nq.BidPrice, nq.PriceType),
-			nxCoreClass.PriceToDouble(nq.AskPriceChange, nq.PriceType),
-			nxCoreClass.PriceToDouble(nq.BidPriceChange, nq.PriceType),
-			nxCoreClass.PriceToDouble(nq.AskSizeChange, nq.PriceType),
-			nxCoreClass.PriceToDouble(nq.BidSizeChange, nq.PriceType));
-	}
+		case NxMSG_EXGQUOTE:
+		{
+			FILE* outfile;
+			outfile = std::fopen("C:\\Users\\Nanex1\\Documents\\output1.txt", "a");
+			const NxCoreQuote& nq = pNxCoreMessage->coreData.ExgQuote.coreQuote;
+			const NxCoreHeader& ch = pNxCoreMessage->coreHeader;
+			const NxTime& t = pNxCoreSys->nxTime;
+			const char* exchange = nxCoreClass.GetDefinedString(NxST_EXCHANGE, ch.ReportingExg);
+			const char* listedexg = nxCoreClass.GetDefinedString(NxST_EXCHANGE, ch.ListedExg);
+			char symbol[23];
+			int lineHour = t.Hour;
+			int lineMinutes = t.Minute;
+			
+			std::string lineTicker = (std::string) ch.pnxStringSymbol->String;
+			if (lineTicker != ticker || (lineHour < startHour) || (lineHour > endHour)) {
+				std::cout << (std::string) ch.pnxStringSymbol->String << " " << ticker << std::endl;
+				std::fclose(outfile);
+				break;
+			}
+			if ((startHour == lineHour && lineMinutes >= startMinutes) ||
+				(endHour == lineHour && lineMinutes <= endMinutes) ||
+				(lineHour > startHour && lineHour < endHour)) {
+				std::cout << "YEEEEEEEEEEEEEEEEEEEEEEET" << std::endl;
+				getSymbol(pNxCoreMessage, symbol);
+				fprintf(outfile, "%.2d:%.2d:%.2d.%.3d, Reporting:%s, Listed:%s, %s, Ask(%ld@%.2lf), Bid(%ld@%.2lf), AskChange(%.2lf), BidChange(%.2lf), AskSizeChange(%.2lf), BidSizeChange(%.2lf)\n",
+					(int)t.Hour, (int)t.Minute, (int)t.Second, (int)t.Millisecond,
+					exchange,
+					listedexg,
+					symbol,
+					nq.AskSize,
+					nxCoreClass.PriceToDouble(nq.AskPrice, nq.PriceType),
+					nq.BidSize,
+					nxCoreClass.PriceToDouble(nq.BidPrice, nq.PriceType),
+					nxCoreClass.PriceToDouble(nq.AskPriceChange, nq.PriceType),
+					nxCoreClass.PriceToDouble(nq.BidPriceChange, nq.PriceType),
+					nxCoreClass.PriceToDouble(nq.AskSizeChange, nq.PriceType),
+					nxCoreClass.PriceToDouble(nq.BidSizeChange, nq.PriceType));
+				std::fclose(outfile);
+			}
+		}
 
 	break;
 	}
@@ -116,7 +147,7 @@ int __stdcall nxCoreCallback(const NxCoreSystem* pNxCoreSys, const NxCoreMessage
 
 int main(int argc, char** argv)
 {	
-	if (!nxCoreClass.LoadNxCore("C:\\Users\\Josh\\source\\repos\\EveryTrade\\NxCoreAPI.dll"))
+	if (!nxCoreClass.LoadNxCore("C:\\Users\\Nanex1\\source\\repos\\CSCProject\\Debug\\NxCoreAPI.dll"))
 	{
 		fprintf(stderr, "Can't find NxCoreAPI.dll\n");
 		system("pause");
@@ -128,20 +159,15 @@ int main(int argc, char** argv)
 	// Place the python script in the directory containing the tape files.
 	// then replace the line below with the formatted array declaration outputted by the python script i.e. std::string files[1] = {"20080101.GS.nx2"};
 	
-	std::vector<std::string> files = { "C:\\Users\\Josh\\source\\repos\\EveryTrade\\EveryTrade\\20080401.GS.nx2" };
-	omp_set_num_threads(2);
-	std::cout << omp_get_num_threads() << " threads running\n";
-	#pragma omp parallel for num_threads(2)
-	for (int i = files.size() - 1; i >= 0; i--) {
-		#pragma omp critical
-		{
-			std::cout << "thread " << omp_get_thread_num() << " using file: " << files[i] << std::endl;
-		}
-		std::string s = files[i];
-		std::cout << s << std::endl;
-		nxCoreClass.ProcessTape(s.c_str(), 0, NxCF_EXCLUDE_CRC_CHECK, 0, nxCoreCallback);
-	}
-
+	file = "C:\\Users\\Nanex1\\source\\repos\\CSCProject\\Debug\\" + (std::string) argv[6];
+	ticker = (std::string) argv[1];
+	startHour = std::stoi((std::string) argv[2]);
+	startMinutes = std::stoi((std::string) argv[3]);
+	endHour = std::stoi((std::string) argv[4]);
+	endMinutes = std::stoi((std::string) argv[5]);
+	std::cout << file << " " << ticker << std::endl;
+	nxCoreClass.ProcessTape(file.c_str(), 0, NxCF_EXCLUDE_CRC_CHECK, 0, nxCoreCallback);
+	
 	system("pause");
 	return 0;
 	
